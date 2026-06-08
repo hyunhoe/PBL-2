2f32d682aaf983bf47889c39460047015f052f21
Yunjae-Jeong <yunjaejeong000@gmail.com>
2026-06-08 20:19:01 +0900

    Beverages 클래스 수정

--- Products.java
+++ Products.java
@@ -10,16 +10,52 @@ public abstract class Products
     protected String code;
     protected String name;
     protected int price;
+
+    /**
+     * Products 클래스의 객체 생성자
+     */
+    public Products(String code, String name, int price)
+    {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+
     /**
      * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
      * @param  y  메소드의 샘플 파라미터
      * @return    x 와 y의 합
      */
-    public int sampleMethod(int y)
+    public String getCode()
     {
         // 여기에 코드를 작성하세요
-        return y;
+        return code;
     }
 
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public String getName()
+    {
+        // 여기에 코드를 작성하세요
+        return name;
+    }
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public int getPrice()
+    {
+        // 여기에 코드를 작성하세요
+        return price;
+    }
+
+    public abstract int calculatePayment();
 }
