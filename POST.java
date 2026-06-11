e6b9e02bf7e0d6413d84955e1b0e6c1f2364511e
hyunhoe <hyunhoe5641@gmail.com>
2026-06-11 17:05:29 +0900

    POST 클래스 뼈대 기입

--- POST.java
+++ POST.java
@@ -8,8 +8,11 @@
 public class POST
 {
     // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
-
+    private double[][] productDB;
+    private String[] productNameDB;
+    private Sale[] saleDB;
+    private int productCount;
+    
     /**
      * POST 클래스의 객체 생성자
      */
@@ -20,12 +23,168 @@ public class POST
     }
 
     /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 상품DB에 바코드, 상품명, 가격, 종류번호를 저장하는 메소드
      *
      * @param  y  메소드의 샘플 파라미터
      * @return    x 더하기 y의 결과값을 반환
      */
-    public int sampleMethod(int y)
+    public void saveProductDB()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 입력한 바코드가 존재하는지 확인하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public int checkBarcode(int code)
+    {
+        // 여기에 코드를 작성하세요.
+        return code;
+    }
+    
+    /**
+     * 상품(음료, 주류) 객체 생성하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public Products createProduct()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 결제 끝난 상품 정보 저장하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public void saveSaleDB(Products product, int quantity, int totalPrice)
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 판매 배열 반환하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public Sale[] getSaleDB()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 판매 건수가 얼마인지 반환하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public int SaleCount()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 결제를 진행하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public void calculateAllTotal()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 입력된 상품 정보를 화면에 출력하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public void printProductInfo()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 모든상품의 결제금액의 합을 계산하는 메소드 
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public double calculateTotalPrice()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 모든 상품의 세금 합계 계산하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public double calculateAllVAT()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 거스름돈 계산하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public double calculateBalance()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 판매한 상품들 DB에 저장하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public void saveAllSaleDB()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 영수증 출력하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public void printReceipt()
+    {
+        // 여기에 코드를 작성하세요.
+        return x + y;
+    }
+    
+    /**
+     * 결제 과정 실행하는 메소드
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 더하기 y의 결과값을 반환
+     */
+    public void runPost()
     {
         // 여기에 코드를 작성하세요.
         return x + y;
