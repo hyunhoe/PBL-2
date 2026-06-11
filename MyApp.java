19c8cd56132aec19bb89a941aea2e55c9bb28eb4
hyunhoe <hyunhoe5641@gmail.com>
2026-06-11 16:19:23 +0900

    MyApp 기초 뼈대 작성

--- MyApp.java
+++ MyApp.java
@@ -7,27 +7,20 @@
  */
 public class MyApp
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
+    public static void main(String[] args){
 
-    /**
-     * MyApp 클래스의 객체 생성자
-     */
-    public MyApp()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
     }
 
     /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
      * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
+     * @return    x 와 y의 합
      */
-    public int sampleMethod(int y)
+    public void runPost()
     {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+        // 여기에 코드를 작성하세요
+        
     }
+
 }
